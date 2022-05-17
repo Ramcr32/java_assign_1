19c28cf4f0070eacb1016ccfa18ae25a136821e2
Ram <ss4033167@gmail.com>
2022-05-17 22:32:04 +0530

    primeFactor

--- src/com/masai/prime_factors.java
+++ src/com/masai/prime_factors.java
@@ -6,9 +6,9 @@ public class prime_factors {
 		// TODO Auto-generated method stub
 		/*The prime factors of a number are all of the integers below that number that are divisible
 		into the number as well as 1. For example, the prime factors of 12 are 1,2,3,4,6, and 12.*/
-		prime(12);
+		primeFactor(12);
 	}
-	public static void prime(int n) {
+	public static void primeFactor(int n) {
 		if(n>100 || n<1) {
 			System.out.println("Invalid number");
 			return ;
