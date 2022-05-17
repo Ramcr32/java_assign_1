779bd7626ef0fdb47b7bb7b12e75296427bcfa50
Ram <ss4033167@gmail.com>
2022-05-17 22:45:59 +0530

    sum of n natural number

---
+++ src/com/masai/SumOfNaturalNumber.java
@@ -0,0 +1,14 @@
+package com.masai;
+
+public class SumOfNaturalNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sumOfNatural(5);
+	}
+	public static void sumOfNatural(int n) {
+		int sum=(n*(n+1))/2;
+		System.out.println(sum);
+	}
+
+}
