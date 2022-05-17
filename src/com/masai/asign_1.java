2f28965954c29c679bfe841a8a6fa77133cb79c3
Ram <ss4033167@gmail.com>
2022-05-17 21:36:47 +0530

    java assignment 1 using switch case

---
+++ src/com/masai/asign_1.java
@@ -0,0 +1,18 @@
+package com.masai;
+
+public class asign_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String city="mumbai";
+		switch (city) {
+		case "mumbai":
+			System.out.println("Financial city");
+			break;
+
+		default:
+			break;
+		}
+	}
+
+}
